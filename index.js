bd93c841e44efc80fd0e35cc6d18d36470ccf291
zeekay <zeekayy@gmail.com>
2012-09-02 22:56:01 -0700

    Update API.

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
-var compiled = null;
+var compiledCss = null,
+    compiledJs  = null;
 
-module.exports = function(opts, cb) {
-  if (typeof opts == 'function') {
-    cb = opts;
-    opts = {};
-  }
-
-  var path = require('path'),
-      recess = require('recess'),
-      bootstrapSrc = path.resolve(__dirname + '/less/bootstrap.less'),
-      responsiveSrc = path.resolve(__dirname + '/less/responsive.less');
-
-  if (compiled) {
-    return cb(null, compiled);
-  }
-
-  recess(bootstrapSrc, {compile: true}, function(err, obj) {
-    if (err) {
-      return cb(err);
+exports.css = function(opts) {
+  return function(cb) {
+    if (typeof opts == 'function') {
+      cb = opts;
+      opts = {};
     }
 
-    compiled = obj.output;
+    var path = require('path'),
+        recess = require('recess'),
+        bootstrapSrc = path.resolve(__dirname + '/less/bootstrap.less'),
+        responsiveSrc = path.resolve(__dirname + '/less/responsive.less');
 
-    if (opts.responsive) {
-      recess(responsiveSrc, {compile: true}, function(err, obj) {
-        if (err) {
-          return cb(err);
-        }
+    if (compiledCss) {
+      return cb(null, compiledCss);
+    }
 
-        compiled += obj.output;
+    recess(bootstrapSrc, {compile: true}, function(err, obj) {
+      if (err) {
+        return cb(err);
+      }
 
-        cb(null, compiled);
-      });
+      compiledCss = obj.output;
+
+      if (opts.responsive) {
+        recess(responsiveSrc, {compile: true}, function(err, obj) {
+          if (err) {
+            return cb(err);
+          }
+
+          compiledCss += obj.output;
+
+          cb(null, compiledCss);
+        });
+      } else {
+        cb(null, compiledCss);
+      }
+    });
+  };
+};
+
+exports.js = function(opts) {
+  return function(cb) {
+    if (typeof opts == 'function') {
+      cb = opts;
+      opts = {};
+    }
+
+    var fs = require('fs'),
+        path = require('path'),
+        jsPath = path.resolve(__dirname + '/js');
+
+    if (compiledJs) {
+      return cb(null, compiledJs);
     } else {
-      cb(null, compiled);
+      compiledJs = '';
     }
-  });
+
+    var selected = function(file) {
+      if (!opts.only) {
+        return true;
+      }
+
+      for (var i=0, length=opts.only.length; i<length; i++) {
+        var pattern = new RegExp('bootstrap-' + opts.only[i] + '.*.js');
+        if (file.match(pattern))
+          return true;
+      }
+      return false;
+    };
+
+    fs.readdir(jsPath, function(err, files) {
+      var count = 0;
+
+      files.filter(function(file) {
+        if (file.match(/bootstrap-.*.js/) && selected(file)) {
+          count += 1;
+
+          fs.readFile(path.join(jsPath, file), function(err, body) {
+            compiledJs += body;
+            count -= 1;
+            if (count === 0) {
+              cb(null, compiledJs);
+            }
+          });
+        }
+      });
+    });
+  };
 };
